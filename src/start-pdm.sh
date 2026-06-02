2ee954bb4b4e8adc8c9928f8c931600c9236b56c
Vichingo455 <vichingo455@gmail.com>
2026-06-02 11:05:06 +0200

    another fix test

--- src/start-pdm.sh
+++ src/start-pdm.sh
@@ -96,8 +96,8 @@ mkdir -p /var/lib/pdm /var/lib/proxmox-datacenter-manager /var/log/proxmox-datac
 chown -R www-data:www-data /var/lib/proxmox-datacenter-manager
 chown -R www-data:www-data /var/lib/pdm
 chown -R root:root /var/log/proxmox-datacenter-manager
-chown -R www-data:www-data /etc/proxmox-datacenter-manager
-chown -R root:www-data /etc/proxmox-datacenter-manager/auth
+chown www-data:www-data /etc/proxmox-datacenter-manager
+chown -R root:www-data /etc/proxmox-datacenter-manager/auth /etc/proxmox-datacenter-manager/access /etc/proxmox-datacenter-manager/subscriptions
 chmod 1770 /etc/proxmox-datacenter-manager
 
 # ============================================================================

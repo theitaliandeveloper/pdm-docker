8a8c1ff9994a9a6b70d11e42c9757fc6f4d58282
Vichingo455 <vichingo455@gmail.com>
2026-06-02 11:07:43 +0200

    another test fix

--- src/start-pdm.sh
+++ src/start-pdm.sh
@@ -95,7 +95,7 @@ log "Ensuring directory permissions..."
 mkdir -p /var/lib/pdm /var/lib/proxmox-datacenter-manager /var/log/proxmox-datacenter-manager
 chown -R www-data:www-data /var/lib/proxmox-datacenter-manager
 chown -R www-data:www-data /var/lib/pdm
-chown -R root:root /var/log/proxmox-datacenter-manager
+chown -R root:www-data /var/log/proxmox-datacenter-manager
 chown -R www-data:www-data /etc/proxmox-datacenter-manager
 chown -R root:www-data /etc/proxmox-datacenter-manager/auth /etc/proxmox-datacenter-manager/access
 chmod 1770 /etc/proxmox-datacenter-manager

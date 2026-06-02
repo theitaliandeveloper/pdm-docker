2d3aa534ed28043edea1a81fd9a10e4e2441af43
Vichingo455 <vichingo455@gmail.com>
2026-06-02 10:57:04 +0200

    test again

--- src/start-pdm.sh
+++ src/start-pdm.sh
@@ -97,6 +97,7 @@ chown -R www-data:www-data /var/lib/proxmox-datacenter-manager
 chown -R www-data:www-data /var/lib/pdm
 chown -R www-data:www-data /var/log/proxmox-datacenter-manager
 chown -R www-data:www-data /etc/proxmox-datacenter-manager
+chown -R www-data:www-data /etc/proxmox-datacenter-manager/auth
 chmod 1770 /etc/proxmox-datacenter-manager
 
 # ============================================================================

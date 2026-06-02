88c293ce1c7f9ab7b5f5f5eed3be9981f526b2e5
Vichingo455 <vichingo455@gmail.com>
2026-06-02 11:30:26 +0200

    another test fix

--- src/start-pdm.sh
+++ src/start-pdm.sh
@@ -92,9 +92,9 @@ fi
 # ============================================================================
 
 log "Ensuring directory permissions..."
-mkdir -p /var/lib/pdm /var/lib/proxmox-datacenter-manager /var/log/proxmox-datacenter-manager /var/log/proxmox-datacenter-manager/api /var/log/proxmox-datacenter-manager/tasks /etc/proxmox-datacenter-manager/access
+mkdir -p /var/lib/pdm /var/lib/proxmox-datacenter-manager /var/log/proxmox-datacenter-manager /var/log/proxmox-datacenter-manager/api /var/log/proxmox-datacenter-manager/tasks /etc/proxmox-datacenter-manager/access /etc/proxmox-datacenter-manager/acme /etc/proxmox-datacenter-manager/acme/accounts
 chown -R www-data:www-data /var/lib/proxmox-datacenter-manager /var/lib/pdm /etc/proxmox-datacenter-manager
-chown -R root:www-data /etc/proxmox-datacenter-manager/auth /etc/proxmox-datacenter-manager/access /var/log/proxmox-datacenter-manager
+chown -R root:www-data /etc/proxmox-datacenter-manager/auth /etc/proxmox-datacenter-manager/access /var/log/proxmox-datacenter-manager /etc/proxmox-datacenter-manager/access /etc/proxmox-datacenter-manager/acme
 chown -R www-data:www-data /var/log/proxmox-datacenter-manager/api /var/log/proxmox-datacenter-manager/tasks
 chmod 1770 /etc/proxmox-datacenter-manager
 chmod 750 /etc/proxmox-datacenter-manager/auth /etc/proxmox-datacenter-manager/access
